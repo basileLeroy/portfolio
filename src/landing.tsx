880aed110bf5204ac242affa8c9f9d61d4cd45cc
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-03-23 18:24:19 +0100

    Fixing upward animation landing page

--- src/landing.tsx
+++ src/landing.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { gsap } from 'gsap';
 import './App.css';
+import { delay } from 'q';
 
 const Landing: React.FC = () => {
 
@@ -10,13 +11,25 @@ const Landing: React.FC = () => {
         tl  .from(".welcome", {opacity:0})
             .fromTo(".welcome", {opacity:0}, {opacity:1 , duration: 1.5}, "-=0.75")
             .to(".welcome", {opacity:0 , duration: 1});
+
+        gsap.fromTo(".landing", {opacity:1 , x:0 , y:0}, {opacity:.5 , x:0 , y:-800 , duration:1.5 , delay:3});
     }
 
     return (
 
         <div className="landingMessage" id="landing">
+            <section>
+                <div className="landing" id="landing-message">
+                    <h1 className="welcome">Welcome!</h1>
+                </div>
+                
+            </section>
 
-            <h1 className="welcome">Welcome!</h1>
+            <section>
+                <div className="container" id="container">
+
+                </div>
+            </section>
             
         </div>
     );
