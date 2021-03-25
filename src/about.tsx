f91b2633ecc41c7ab758f0bacb6151f39ba7810e
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-03-25 23:24:01 +0100

    Correcting text in about page

--- src/about.tsx
+++ src/about.tsx
@@ -10,8 +10,8 @@ const AboutMe: React.FC = () => {
                 <h1>About myself</h1>
                 <p>
                     Not too long ago, I had close to no knowledge about programming.
-                    Now, I have learned the ability to create amazing things in multiple languages!
-                    How far I have gotten up until now, I can only descripbe as a mix of
+                    Now, I have mastered the ability to create amazing things in multiple languages!
+                    How far I have gotten up until now, I can only explain by a mix of
                     interest, wanting to learn, motivation and dedication. <br/><br/>
                     <i>Sociable, teamplayer, client-oriented, .. Those are badges I wear with honor.</i>
                 </p>
