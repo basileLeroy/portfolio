e385291fe73681dfdb54d94aa179261b5604a9ad
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-11-03 12:30:30 -0700

    Update css font rules

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,3 +1,11 @@
 <x-guest-layout>
-
+    <h1>PHP/Webdeveloper - Basile Leroy</h1>
+    <section class="banner">
+        <article class="about-me">
+            <h2>A little about myself</h2>
+        </article>
+        <div class="my-picture">
+            <img src="{{ asset('storage/profile.png') }}" alt="">
+        </div>
+    </section>
 </x-guest-layout>
