548e576b17cd6e186e5ec638aedaa495362888c9
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-11-03 22:49:53 -0600

    Create About me section

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,8 +1,11 @@
 <x-guest-layout>
-    <h1>PHP/Webdeveloper - Basile Leroy</h1>
     <section class="banner">
         <article class="about-me">
-            <h2>A little about myself</h2>
+            <h1><span class="focus-big">PHP</span>/<span class="focus-big">Web</span>developer</h1>
+            <h2>Hey There! I am</h2>
+            <p class="name">Basile Leroy from Belgium</p>
+            <p>I am a backend developer but but have been doing full stack projects too. Primarily I focus on <span class="focus-normal">PHP</span>/<span class="focus-normal">Laravel</span>. My career started as a self thaught developer but I ended joining a training to increase my skills and knowledge.</p>
+            <p>Next, I was able to work on a few projects. But soon, I got invited to be a <span class="focus-normal">coach</span>/<span class="focus-normal">trainer</span> at the same organisation where I was trained. Since then I have been training jobseekers in Belgium to find a job as a webdeveloper.</p>
         </article>
         <div class="my-picture">
             <img src="{{ asset('storage/profile.png') }}" alt="">
