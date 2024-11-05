726bd0a31d3a93b5e41894b3bacb20763ae6cd9b
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-11-04 19:01:53 -0600

    Replace logo with animated logo for desktop

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -1,7 +1,14 @@
 <header>
-    <a href="/" id="logo">
-        <img src="{{ asset('storage/logo.png') }}" alt="">
-        <span>asile Leroy</span>
+    <a href="/" id="logo" class="desktop">
+        <video autoplay muted playsinline width="175">
+            <source src="{{ asset('storage/logo-animation.webm') }}" type="video/webm">
+            Your browser does not support the video tag.
+        </video>
     </a>
+
+    <a href="/" id="logo" class="mobile">
+        <img src="{{ asset('storage/logo.png') }}" alt="">
+    </a>
+    
     <x-nav />
 </header>
