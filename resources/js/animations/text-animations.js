a3a52809010f60fc959e2a334967a7f7e649b6dc
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-11-07 13:00:24 -0600

    Add section observer and animations: fade in, update textanimation

--- resources/js/animations/text-animations.js
+++ resources/js/animations/text-animations.js
@@ -3,25 +3,67 @@ import "splitting/dist/splitting-cells.css";
 import Splitting from "splitting";
 import { gsap } from "gsap";
 
-window.addEventListener('DOMContentLoaded', () => {
-    const targets = document.querySelectorAll('.quote');
-    const allLines = [];
+const sectionVisibleCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.toggle("show", entry.isIntersecting);
 
-    targets.forEach((target) => {
-        if (target) {
-            const results = Splitting({ target: target, by: 'lines' });
+            const targets = entry.target.querySelectorAll('.quote');
+            
+            const allLines = [];
 
-            results[0].lines.forEach((line) => {
-                allLines.push(...line);
+            targets.forEach((target) => {
+                if (target) {
+                    const results = Splitting({ target: target, by: 'lines' });
+        
+                    results[0].lines.forEach((line) => {
+                        allLines.push(...line);
+                    });
+                } else {
+                    console.error("Element with ID 'quote' not found");
+                }
             });
-        } else {
-            console.error("Element with ID 'quote' not found");
+        
+            allLines.forEach((word, index) => {
+                if (word instanceof HTMLElement) {
+                    gsap.from(word, { opacity: 0, delay: index / 100, duration: 0.2 });
+                }
+            });
+            observer.unobserve(entry.target);
         }
-    });
+    })
+}
+const options = {
+    threshold: 0.5
+};
 
-    allLines.forEach((word, index) => {
-        if (word instanceof HTMLElement) {
-            gsap.from(word, { opacity: 0, delay: index / 100, duration: 0.2 });
-        }
-    });
+const observer = new IntersectionObserver(sectionVisibleCallback, options);
+
+const sections = document.querySelectorAll('section');
+sections.forEach(section => {
+    observer.observe(section); // Start observing each section
 });
+
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const targets = document.querySelectorAll('.quote');
+//     const allLines = [];
+
+//     targets.forEach((target) => {
+//         if (target) {
+//             const results = Splitting({ target: target, by: 'lines' });
+
+//             results[0].lines.forEach((line) => {
+//                 allLines.push(...line);
+//             });
+//         } else {
+//             console.error("Element with ID 'quote' not found");
+//         }
+//     });
+
+//     allLines.forEach((word, index) => {
+//         if (word instanceof HTMLElement) {
+//             gsap.from(word, { opacity: 0, delay: index / 100, duration: 0.2 });
+//         }
+//     });
+// });
