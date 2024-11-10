e8327bf56f456882aa1a831481a8d42cfb1822a7
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-11-10 16:55:11 -0500

    Fix typo in section heading

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,7 +6,7 @@
         <h1><span class="focus-big">PHP</span>/<span class="focus-big">Web</span>developer</h1>
         <div>
             <article class="about-me">
-                <h2>Hey There! I am</h2>
+                <h2>Hey there! I am</h2>
                 <p class="name">Basile Leroy from Belgium</p>
                 <p class="quote">I am a backend developer but have been doing full stack projects too. Primarily I focus on <span class="focus-normal">PHP</span>/<span class="focus-normal">Laravel</span>. My career started as a self-taught developer but I ended up joining a training program to increase my skills and knowledge.</p>
                 <p class="quote">Next, I was able to work on a few projects. But soon, I got invited to be a <span class="focus-normal">coach</span>/<span class="focus-normal">trainer</span> at the same organization where I was trained. Since then, I have been training job seekers in Belgium to find a job as a web developer.</p>
