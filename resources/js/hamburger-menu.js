9623118ff80c3323a59f7944500ab3032fac658c
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-12-01 15:42:29 +0100

    Refactor mobile nav update

--- resources/js/hamburger-menu.js
+++ resources/js/hamburger-menu.js
@@ -5,16 +5,3 @@ burger.addEventListener('click', function(e){
 	this.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
 });
-
-
-
-// /* Onload demo - dirty timeout */
-// let clickEvent = new Event('click');
-
-// window.addEventListener('load', function(e) {
-// 	burger.dispatchEvent(clickEvent);
-	
-// 	// setTimeout(function(){
-// 	// 	burger.dispatchEvent(clickEvent);
-// 	// }, 5500);
-// });
