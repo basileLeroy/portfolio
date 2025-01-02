7aa2e86378545bddcdec37eab9690a23e9136603
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-02 14:53:49 +0100

    Add javascript for accordion dropdown

--- resources/js/components/accordion.js
+++ resources/js/components/accordion.js
@@ -1,21 +1,15 @@
 const accordionButtons = document.querySelectorAll("button.accordion");
 
 accordionButtons.forEach(button => {
-    console.log(button)
-    button.addEventListener('click', () => {
-        console.log("click");
-        
+    button.addEventListener("click", () => {
+        console.log("clicked");
+        button.classList.toggle("active");
+        let content = button.nextElementSibling;
+        console.log(content)
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
     })
-    // button.addEventListener("click", () => {
-    //     console.log("clicked");
-        
-    //     this.classList.toggle("active");
-    //     let content = this.nextElementSibling;
-    //     console.log(content)
-    //     if (content.style.display === "block") {
-    //         content.style.display = "none";
-    //     } else {
-    //         content.style.display = "block";
-    //     }
-    // })
 })
