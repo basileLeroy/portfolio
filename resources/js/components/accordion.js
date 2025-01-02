fbbf9626ccc912cad2dc2bce8b692dbc4c03f42d
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-02 14:54:18 +0100

    Removing console logs

--- resources/js/components/accordion.js
+++ resources/js/components/accordion.js
@@ -2,10 +2,8 @@ const accordionButtons = document.querySelectorAll("button.accordion");
 
 accordionButtons.forEach(button => {
     button.addEventListener("click", () => {
-        console.log("clicked");
         button.classList.toggle("active");
         let content = button.nextElementSibling;
-        console.log(content)
         if (content.style.display === "block") {
             content.style.display = "none";
         } else {
