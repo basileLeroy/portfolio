a748999bcdf20b68328321d3ce100d4546059ee8
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-02 18:56:47 +0100

    Add new topic to the "about me" section

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -19,6 +19,8 @@
                     class="active">Basile Leroy from Belgium</li>
                 <li id="php-button" tabindex="0" aria-label="show article about my web dev skills" data-topic="php">A
                     PHP web developer</li>
+                <li id="career-button" tabindex="0" aria-label="show article about my career" data-topic="career">My
+                    career as a developer</li>
             </ul>
             <div class="slider-content">
                 <article id="about" class="active" aria-labelledby="about-button">
@@ -33,6 +35,14 @@
                     <p>Of course, I am also familiar with other web oriented programming languages, and I have to admit
                         I even enjoy learning new ones if/when the opportunity is given!</p>
                 </article>
+                <article id="career" aria-labelledby="career-button">
+                    <p>My career started as a self-taught developer but I ended up joining a training program to
+                        increase my skills and knowledge.</p>
+                    <p>Next, I was able to work on a few projects. But soon, I got invited to be a <span
+                            class="focus-normal">coach</span>/<span class="focus-normal">trainer</span> at the same
+                        organization where I was trained. Since then, I have been training job seekers in Belgium to
+                        find a job as a web developer.</p>
+                </article>
             </div>
         </div>
 
@@ -40,15 +50,27 @@
             <button class="accordion">Basile Leroy from Belgium</button>
             <div class="panel">
                 <p>Born and raised in <span class="focus-normal">Belgium</span>. I was brought up in both <span
-                        class="focus-normal">dutch</span> and <span class="focus-normal">french</span> at home but
-                    quickly became fluent in <span class="focus-normal">english</span> too by traveling around and
-                    interacting with people all over the world.</p>
+                    class="focus-normal">dutch</span> and <span class="focus-normal">french</span> at home but
+                quickly became fluent in <span class="focus-normal">english</span> too by traveling around and
+                interacting with people all over the world.</p>
             </div>
 
             <button class="accordion">A PHP web developer</button>
             <div class="panel">
-                <p>As a web developer, I chose to focus on <span class="focus-normal">PHP</span> as a programming
-                    language. </p>
+                <p>As a web developer, I mainly work with <span class="focus-normal">PHP</span> as a programming
+                    language and <span class="focus-normal">Laravel</span> as a framework.</p>
+                <p>Of course, I am also familiar with other web oriented programming languages, and I have to admit
+                    I even enjoy learning new ones if/when the opportunity is given!</p>
+            </div>
+
+            <button class="accordion">My career as a developer</button>
+            <div class="panel">
+                <p>My career started as a self-taught developer but I ended up joining a training program to
+                    increase my skills and knowledge.</p>
+                <p>Next, I was able to work on a few projects. But soon, I got invited to be a <span
+                        class="focus-normal">coach</span>/<span class="focus-normal">trainer</span> at the same
+                    organization where I was trained. Since then, I have been training job seekers in Belgium to
+                    find a job as a web developer.</p>
             </div>
         </div>
 
