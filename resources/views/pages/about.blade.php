c4271cae3ed2f54a06596c5288c553f43054b56b
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-02 11:25:05 +0100

    Update structure Hero component

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,14 @@
 
 <x-guest-layout>
     <section class="hero">
-        <h1 class="hero"><span id="first-name">BASILE</span><br><span id="last-name">LEROY</span></h1>
+        <h1 class="hero">
+            <span id="first-name">BASILE</span><span id="last-name">LEROY</span>
+        </h1>
+        <div class="my-picture">
+            <img src="{{ asset('storage/profile.png') }}" title="Photo of Basile Leroy" alt="A picture of Basile Leroy sitting down with a colorfull background">
+        </div>
+
+
     </section>
     <section class="banner">
         <h1><span class="focus-big">PHP</span>/<span class="focus-big">Web</span>developer</h1>
@@ -14,9 +21,7 @@
                 <p class="quote">I am a backend developer and have been doing full stack projects too. Primarily I focus on <span class="focus-normal">PHP</span>/<span class="focus-normal">Laravel</span>. My career started as a self-taught developer but I ended up joining a training program to increase my skills and knowledge.</p>
                 <p class="quote">Next, I was able to work on a few projects. But soon, I got invited to be a <span class="focus-normal">coach</span>/<span class="focus-normal">trainer</span> at the same organization where I was trained. Since then, I have been training job seekers in Belgium to find a job as a web developer.</p>
             </article>
-            <div class="my-picture">
-                <img src="{{ asset('storage/profile.png') }}" title="Photo of Basile Leroy" alt="A picture of Basile Leroy sitting down with a colorfull background">
-            </div>
+
         </div>
     </section>
     <section class="tech">
