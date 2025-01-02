ecb1adf2712d8dc6ed6fa32a104be52d4de31c8b
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-02 17:59:52 +0100

    Update logo for accessibility - adding area label and alt text

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -1,13 +1,13 @@
 <header>
-    <a href="/" id="logo" class="desktop">
+    <a href="/" id="logo" class="desktop" aria-label="Go to Home - Basile Leroy">
         <video autoplay muted playsinline width="175">
             <source src="{{ asset('storage/logo-animation.webm') }}" type="video/webm">
             Your browser does not support the video tag.
         </video>
     </a>
 
-    <a href="/" id="logo" class="mobile">
-        <img src="{{ asset('storage/logo.png') }}" alt="">
+    <a href="/" id="logo" class="mobile" aria-label="Go to Home - Basile Leroy">
+        <img src="{{ asset('storage/logo.png') }}" alt="Logo of basileleroy.com">
     </a>
     
     <x-nav />
