d2a732a8b7710f8ecf5d11c6c05d4db2a9c4687f
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-02 21:49:27 +0100

    Refactor disabled code

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -73,16 +73,6 @@
                     find a job as a web developer.</p>
             </div>
         </div>
-
-        {{-- <div>
-            <article class="about-me">
-                <h2>Hey there! I am</h2>
-                <p class="name">Basile Leroy from Belgium</p>
-                <p class="quote">I am a backend developer and have been doing full stack projects too. Primarily I focus on <span class="focus-normal">PHP</span>/<span class="focus-normal">Laravel</span>. My career started as a self-taught developer but I ended up joining a training program to increase my skills and knowledge.</p>
-                <p class="quote">Next, I was able to work on a few projects. But soon, I got invited to be a <span class="focus-normal">coach</span>/<span class="focus-normal">trainer</span> at the same organization where I was trained. Since then, I have been training job seekers in Belgium to find a job as a web developer.</p>
-            </article>
-
-        </div> --}}
     </section>
     <section class="tech">
         <fieldset>
