ad0770a261fe771b94c70842f6857d92fe08fd39
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-02 15:36:58 +0100

    Update accordion to have transition animation

--- resources/js/components/accordion.js
+++ resources/js/components/accordion.js
@@ -4,10 +4,15 @@ accordionButtons.forEach(button => {
     button.addEventListener("click", () => {
         button.classList.toggle("active");
         let content = button.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
+        // if (content.style.display === "block") {
+        //     content.style.display = "none";
+        //     if (content.classList.contains("show-content")) {
+        //         content.classList.remove("show-content");
+        //     }        
+        // } else {
+        //     content.style.display = "block";
+        //     content.classList.remove("show-content");
+        // }
+        content.classList.toggle('show-content')
     })
 })
