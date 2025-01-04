db3885feab0e08318395900fd6c91c52700b6dfd
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-04 20:20:06 +0100

    Update projects page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Basile Leroy') }}</title>
+        <title>{{ config('app.name', 'Basile Leroy') }} - @yield("title", '')</title>
 
         {{-- Icons --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -1,3 +1,17 @@
-<x-guest-layout>
+@section("title")
+    Projects
+@endsection
 
+<x-guest-layout>
+    <section class="show projects">
+        <h1>My projects</h1>
+        <p>Here are some projects that I worked on.
+        <hr>
+        <article class="project">
+            <div class="details">
+                <h2>Custom CMS-like dashboard</h2>
+            </div>
+            <div class="visuals"></div>
+        </article>
+    </section>
 </x-guest-layout>
