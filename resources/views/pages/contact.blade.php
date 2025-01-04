8406378d9182297c5c6a7fdfc717f274a9cc7fb9
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-04 19:51:32 +0100

    Update contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,3 +1,24 @@
 <x-guest-layout>
+    <section class="show contact">
+        <form action="">
+            <h1>Contact me</h1>
+            @csrf
+            <label>
+                Name
+                <input type="text" id="name" name="name" placeholder="John Wick" required>
+            </label>
 
+            <label>
+                email
+                <input type="email" id="email" name="email" placeholder="babayaga@mail.com" required>
+            </label>
+
+            <label>
+                Message
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="Your message ..." required></textarea>
+            </label>
+
+            <button>Send !</button>
+        </form>
+    </section>
 </x-guest-layout>
