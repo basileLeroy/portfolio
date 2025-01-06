27475bf1d87eb7108864a0aa2b73001dbc3a6bd0
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-06 11:52:11 +0100

    Add new projects to /projects

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -59,5 +59,52 @@
                 </a>
             </div>
         </article>
+        <article class="project">
+            <h2>Game Guide platform for game community</h2>
+            <div class="details">
+                <p>One of the first websites (static) I made: an information/guide platform made for a game community about a certain game.</p>
+                <p>The website is not live anymore, but I took some screen captures from a local version.</p>   
+                <h3>Features include:</h3>
+                <ul class="features">
+                    <li>A complex bracket calculator helping players to know what combinations of levels they need</li>
+                    <li>Gear suggestions based on type</li>
+                    <li>documentation displayed in a fun/readable way</li>
+                </ul>
+                <h3>Stack</h3>
+                <ul class="tech">
+                    <li>ReactJS</li>
+                    <li>SCSS/SASS</li>
+                </ul>
+            </div>
+            <div class="visuals pswp-gallery" id="my-gallery">
+                <a href={{ asset('storage/projects/hustle-prime/arena-demo.gif') }} 
+                    data-pswp-width="994" 
+                    data-pswp-height="585" 
+                    target="_blank">
+                    <img src={{ asset('storage/projects/hustle-prime/arena-demo.jpg') }} alt="Thumbnail 1">
+                </a>
+                <a href={{ asset('storage/projects/hustle-prime/banner-example.jpg') }} 
+                    data-pswp-width="1000" 
+                    data-pswp-height="500" 
+                    target="_blank">
+                    <img src={{ asset('storage/projects/hustle-prime/banner-example.jpg') }} alt="Thumbnail 2">
+                </a>
+                <a href={{ asset('storage/projects/hustle-prime/dynamic-components.gif') }}
+                    data-pswp-width="994" 
+                    data-pswp-height="585"
+                    target="_blank">
+                    <img src={{ asset('storage/projects/hustle-prime/dynamic-components.jpg') }}
+                        alt="Thumbnail 3">
+                </a>
+                </a>
+                <a href={{ asset('storage/projects/hustle-prime/gembay-demo.gif') }}
+                    data-pswp-width="994" 
+                    data-pswp-height="585"
+                    target="_blank">
+                    <img src={{ asset('storage/projects/hustle-prime/gembay-demo.jpg') }}
+                        alt="Thumbnail 3">
+                </a>
+            </div>
+        </article>
     </section>
 </x-guest-layout>
