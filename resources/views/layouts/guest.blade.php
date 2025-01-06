e552e245726610a24cd3138947324c208c85dfc1
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-01-06 14:37:00 +0100

    Add meta content for Meta cards

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,8 +4,27 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="description" content="@yield('description')">
 
+        {{-- Meta Cards - Default --}}
+        <meta name="author" content="Basile Leroy">
+        <meta name="description" content="@yield('description')">
+        <meta name="keywords" content="Basile Leroy, Portfolio, website, about, contact, projects, SEO, Social Media, Meta">
+
+        {{-- Meta Cards - OpenGraph --}}
+        <meta property="og:title" content="{{ config('app.name', 'Basile Leroy') }} - @yield('title', '')">
+        <meta property="og:description" content="@yield('description')">
+        <meta property="og:image" content={{ asset("storage/meta-pic.jpg") }}>
+        <meta property="og:url" content="basileleroy.com">
+        <meta property="og:type" content="website">
+
+        {{-- Meta Cards - Twitter --}}
+        <meta name="twitter:card" content={{ asset("storage/meta-pic.jpg") }}>
+        <meta name="twitter:title" content="{{ config('app.name', 'Basile Leroy') }} - @yield('title', '')">
+        <meta name="twitter:description" content="@yield('description')">
+        <meta name="twitter:image" content={{ asset("storage/meta-pic.jpg") }}>
+
+        {{-- Favicon & Page Title --}}
+        <link rel="icon" href={{ asset("storage/logo.ico") }} type="image/x-icon">
         <title>{{ config('app.name', 'Basile Leroy') }} - @yield("title", '')</title>
 
         {{-- Icons --}}
@@ -18,7 +37,6 @@
 
         {{-- API's --}}
         <script src="https://www.google.com/recaptcha/api.js"></script>
-
 
         <!-- Scripts -->
         @vite(['resources/js/app.js'])
