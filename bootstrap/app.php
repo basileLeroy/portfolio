15e03abfa7f03d405ef2f3e62568c98da5156049
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-03-01 16:46:37 +0100

    Remove forced 'trailing /' urls

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\ForceTrailingSlash;
 use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -14,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(SecurityHeaders::class);
-        $middleware->append(ForceTrailingSlash::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
