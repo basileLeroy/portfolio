564ef5fec82552292d7b79993f59b406df829cfe
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-03-01 16:26:36 +0100

    Add forced redirects to trail url with /

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ForceTrailingSlash;
 use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(SecurityHeaders::class);
+        $middleware->append(ForceTrailingSlash::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
