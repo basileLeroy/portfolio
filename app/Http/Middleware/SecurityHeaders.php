baf245f6449e169a9b80e61daf654947f7dc4c22
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-03-01 15:29:55 +0100

    Remove Csp class from middleware

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -5,8 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Spatie\Csp\Csp;
-
 
 class SecurityHeaders
 {
@@ -27,15 +25,14 @@ class SecurityHeaders
 
         // Apply CSP only in production
         if (app()->environment('production')) {
-            $csp = new Csp();
-            $csp->addDirective('default-src', "'self'")
-                ->addDirective('style-src', "'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.bunny.net")
-                ->addDirective('script-src', "'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://cloud.umami.is")
-                ->addDirective('font-src', "'self' https://fonts.bunny.net https://cdnjs.cloudflare.com")
-                ->addDirective('img-src', "'self' data:")
-                ->addDirective('frame-src', "'self' https://www.google.com/");
+            $csp = "default-src 'self'; " .
+                "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.bunny.net; " .
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://cloud.umami.is; " .
+                "font-src 'self' https://fonts.bunny.net https://cdnjs.cloudflare.com; " .
+                "img-src 'self' data:; " .
+                "frame-src 'self' https://www.google.com/";
 
-            $response->headers->set("Content-Security-Policy", $csp->renderHeader());
+            $response->headers->set("Content-Security-Policy", $csp);
         }
 
         return $response;
