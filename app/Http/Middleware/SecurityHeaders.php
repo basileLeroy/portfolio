e6fa8091b97cf662f1c2578c6c0d879e7229a49c
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-03-01 15:24:21 +0100

    Fix namespace error

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Spatie\Csp\AddCspHeaders;
+use Spatie\Csp\Csp;
 
 
 class SecurityHeaders
