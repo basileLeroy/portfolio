c818c886c4d036fbb29626017afd0d357d4b4453
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-03-04 14:19:48 +0100

    Add aria-labels to social media links

--- resources/views/components/footer/index.blade.php
+++ resources/views/components/footer/index.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="social">
-        <a href="https://www.linkedin.com/in/basile-leroy/"><i class="icon ion-social-linkedin"></i></a>
-        <a href="https://github.com/basileLeroy"><i class="icon ion-social-github"></i></a>
+        <a href="https://www.linkedin.com/in/basile-leroy/" aria-label="Linkedin"><i class="icon ion-social-linkedin"></i></a>
+        <a href="https://github.com/basileLeroy" aria-label="Github"><i class="icon ion-social-github"></i></a>
     </div>
     <ul class="footer-navigation">
         <li class="link"><a href={{ route("about")}}>Home</a></li>
